0ed74134a332162d59ca14e56c6fee0e00c38200
Steven McVey <smcvey1210@gmail.com>
2023-02-16 13:51:41 -0800

    commit tree 2f72d72d408fb3a384f7ccdfc9b9d5ce39623d6e

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,22 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
-    render json: students
+    if params[:name]
+
+      student_by_name = Student.select do |student|
+        query = params[:name].downcase
+        student.first_name.downcase == query || student.last_name.downcase == query
+      end
+
+      render json: student_by_name
+    else
+      render json: students
+    end
+
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
 end
